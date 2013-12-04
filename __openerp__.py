c0396035f06efa3a1f100f0515374171e9e73463
Fareed <blueskies_fareed@hotmail.com>
2013-12-04 11:47:02 +0300

    First  Commit

---
+++ __openerp__.py
@@ -0,0 +1,28 @@
+
+{
+    'name': 'Test',
+    'version': '1.1',
+    'author': 'Afras Trading & Corporating',
+    'category': 'Test Category',
+    'depends': ['base'],
+    'demo': [],
+    'description': """ The Practise Module
+    """,
+    'data': [
+#        'security/product_security.xml',
+#        'security/ir.model.access.csv',
+#        'wizard/product_price_view.xml',
+#        'product_data.xml',
+#        'product_report.xml',
+        'test_view.xml',
+#        'product_shortcut_data.xml',
+#        'pricelist_view.xml',
+#        'partner_view.xml',
+#        'process/product_process.xml'
+    ],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+#    'images': ['images/product_uom.jpeg','images/product_pricelists.jpeg','images/products_categories.jpeg', 'images/products_form.jpeg'],
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ test.py
@@ -0,0 +1,36 @@
+from openerp.osv import osv, fields
+from datetime import datetime,time
+
+class test(osv.osv):
+    """Test Class"""
+
+    _name = 'test'
+    _description = 'test'
+    
+    _columns = {
+        'name': fields.char('Name', size=64, required=True , tranlsate = True),
+        'date': fields.date('Date',),
+        'city': fields.char('City',required=True, size=50),
+        'state':fields.char('State',required=True, size=50),
+        'zip'  :fields.char('Zip', size=50),   
+        'lines' : fields.one2many('test.child','test_ids',ondelete = 'cascade'),  
+    }
+    
+    _defaults = {
+    
+                    'date'   : lambda *a: datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+    }
+test()   
+
+class test_child(osv.osv):
+	"""Test Child"""		
+	
+	_name = 'test.child'
+	_description = 'Test Child'
+	_columns = {
+		'test_ids': fields.many2one('test','TEstChild',hidden=True),
+		'name' : fields.char('Name',size=5,required=True),
+		'desc' : fields.text('Description',size=10),
+			}		
+test_child()	
+	
